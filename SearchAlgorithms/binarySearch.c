ab5d6c86bb484d5a0043208b1730de890e69613b
Julian Ley <contact@julianley.com>
2024-06-25 22:18:19 +0200

    Binary Search
    Added code for a simple Binary Search implementation.

--- SearchAlgorithms/binarySearch.c
+++ SearchAlgorithms/binarySearch.c
@@ -1,6 +1,41 @@
 #include <stdio.h>
 
+int binarySearch (int* array, int size, int target)
+{
+    int left = 0;
+    int right = size-1;
+
+    while (left <= right)
+    {
+        int middle = left + (right - left) / 2;
+        if (array[middle] < target)
+        {
+            left = middle + 1;
+        }
+        else if (array[middle] > target)
+        {
+            right = middle -1;
+        }
+        else
+        {
+            return middle;
+        }
+    }
+
+    return -1;
+}
+
+
 int main()
 {
+    //Example
+
+    int array[] = {1, 2, 3, 4, 5, 7, 9};
+    int target = 4;
+    int arrayLength = sizeof(array) / sizeof(array[0]);
+
+    int result = binarySearch(array, arrayLength, target);
+    printf("Result = %d\n", result);
+
     return 0;
 }
